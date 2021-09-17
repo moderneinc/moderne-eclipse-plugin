2b1d75e6fa8b1644e1b7d026c34ac1fbe5f68922
Jonathan Schneider <jkschneider@gmail.com>
2021-09-16 21:26:01 -0700

    [wip]

---
+++ src/io/moderne/eclipse/FindUsagesAction.java
@@ -0,0 +1,20 @@
+package io.moderne.eclipse;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IActionDelegate;
+
+public class FindUsagesAction extends Action implements IActionDelegate {
+
+	@Override
+	public void run(IAction arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectionChanged(IAction arg0, ISelection arg1) {
+		// TODO Auto-generated method stub
+	}
+}
--- src/io/moderne/eclipse/ModerneHandler.java
+++ src/io/moderne/eclipse/ModerneHandler.java
@@ -9,6 +9,12 @@ import org.eclipse.jface.dialogs.MessageDialog;
 
 public class ModerneHandler extends AbstractHandler {
 
+	// https://books.google.com/books?id=Uo4SlCvSy40C&pg=PA41&lpg=PA41&dq=eclipse+plugin+add+to+refactor+popup&source=bl&ots=No6LFtfSaa&sig=ACfU3U35a7bnIMske3roPhClaK2hYiF6-Q&hl=en&sa=X&ved=2ahUKEwinnOSqj4XzAhVlNn0KHfY6DfkQ6AF6BAgUEAM#v=onepage&q=eclipse%20plugin%20add%20to%20refactor%20popup&f=false
+	
+	// https://www.eclipse.org/forums/index.php/t/94198/
+	
+	// org.eclipse.jdt.core.IMember
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 //		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
